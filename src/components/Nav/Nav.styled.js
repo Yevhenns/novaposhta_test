2ab3b359cea57e096e4552dd7a17b42de155b139
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2023-03-03 18:44:28 +0200

    mobile nav

--- src/components/Nav/Nav.styled.js
+++ src/components/Nav/Nav.styled.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 export const Link = styled(NavLink)`
   color: black;
-  font-size: 30px;
+  font-size: 10px;
   border: 1px solid;
   border-radius: 5px;
   padding: 5px;
@@ -14,11 +14,16 @@ export const Link = styled(NavLink)`
     color: white;
     box-shadow: 10px 5px 5px grey;
   }
+  @media screen and (min-width: 768px) {
+    font-size: 20px;
+  }
 `;
 export const Nav = styled.nav`
   text-align: center;
   background-color: #1976d2;
   padding: 20px;
+  border-bottom-left-radius: 100px 50px;
+  border-bottom-right-radius: 100px 50px;
 `;
 export const List = styled.ul`
   justify-content: center;
